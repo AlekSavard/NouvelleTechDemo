f52ee96661d47960ef90898159e41b6d5b7ff1cf
AlekSavard <aleksavard@hotmail.com>
2018-09-05 11:42:50 -0400

    Commit initial

---
+++ TechDemo/TechDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
